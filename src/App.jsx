6b47a5090021433585f3548ad8861741e30f9ee9
rcastillod <rodrigo.castillo@global3.cl>
2023-07-24 09:03:03 -0400

    feat: :construction: Working on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+// Assets
+import rose from "./assets/images/rose.png";
+
 const App = () => {
   return (
     <main>
-      <div className="max-w-3xl mx-auto">Hola</div>
+      <div className="max-w-3xl mx-auto px-4">
+        <div className="py-10 min-h-screen">
+          <div className="card__wrapper py-24">
+            <h2 className="font-light uppercase tracking-widest text-center">
+              Estas <span className="font-bold">invitado</span> a mi
+            </h2>
+            <div className="card__image flex justify-center my-7 relative z-10">
+              <img src={rose} alt="" />
+            </div>
+            <div className="card__title font-serif text-center uppercase -mt-8 md:-mt-11">
+              Fiesta de <br />
+              <span>Cumpleaños</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFE5B2',
+        secondary: '#8B6131',
+        'dark-grey': '#111018',
+        'light-grey': '#26252C',
+        'body-color': '#F8F1F9'
+      },
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        serif: ["vogamedium", 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
